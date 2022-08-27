a57b38188cacf01c5881b4f3a300e520afe0035e
Janek Mann <janek.mann@knaufenergy.com>
2022-08-27 22:41:59 +0100

    add s3 upload suppot

--- install_script.sh
+++ install_script.sh
@@ -1,19 +1,6 @@
 apt-get update
-apt-get --assume-yes install screen zip libglib2.0-0 libsm6 libxext6 libxrender1
-wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.12.0-Linux-x86_64.sh
-chmod a+x Miniconda3-latest-Linux-x86_64.sh
-./Miniconda3-latest-Linux-x86_64.sh -b -p /workspace/conda
-/workspace/conda/bin/conda init
-cp /root/.bashrc /workspace/.bashrc
-. /workspace/.bashrc
-if [ ! -f /path/to/file ]; then
-    wget $SD_MODEL_URL
-fi
+apt-get --assume-yes install screen zip
 git clone https://github.com/janekm/progrock-stable.git
-cd progrock-stable
-conda env create -f environment.yaml
-conda activate prs
-cd ..
 git clone https://github.com/xinntao/Real-ESRGAN.git
 cd Real-ESRGAN
 pip install -r requirements.txt
@@ -26,4 +13,6 @@ wget $IN_FILES_ZIP_URL
 unzip gobig_in.zip
 ln ../model.ckpt models/sd-v1-4.ckpt
 python prs.py --gobig_dir gobig_in -s my_settings.json
+zip out.zip ./out/Default/*
+
 # bash -c "sleep infinity"
